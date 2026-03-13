24b44e053190b94f59fa00d712bab9ca20d6e892
RohanAM6606 <rohan02aug@gmail.com>
2026-03-13 13:04:33 +0530

    UC4:Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -28,26 +28,49 @@ public class PalindromeCheckerApp {
         System.out.println("      USE CASE 3: Reverse String Palindrome Check");
         System.out.println("==============================================");
 
-        // Hardcoded input string
         input = "LEVEL";
 
-        // Empty string to store reversed value
         String reversed = "";
 
         System.out.println("Original String: " + input);
 
-        // Iterate from the last character to the first
         for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i); // string concatenation
+            reversed = reversed + input.charAt(i);
         }
 
         System.out.println("Reversed String: " + reversed);
 
-        // Compare strings using equals()
         if (input.equals(reversed)) {
             System.out.println("Result: \"" + input + "\" IS a palindrome.");
         } else {
             System.out.println("Result: \"" + input + "\" is NOT a palindrome.");
         }
+
+        System.out.println("==============================================");
+        System.out.println("      USE CASE 4: Character Array Based Palindrome Check");
+        System.out.println("==============================================");
+
+        input = "radar";
+        System.out.println("Input String: " + input);
+
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+
+        int end = chars.length - 1;
+
+        isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Is Palindrome?: " + isPalindrome);
     }
 }
